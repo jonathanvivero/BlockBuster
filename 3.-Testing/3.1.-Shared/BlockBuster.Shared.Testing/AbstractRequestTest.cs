db7381b8b12917ef50e5b9092282479ede8d826d
Jonathan Vivero <ext.jonathan.viverogazquez@aareon.com>
2021-04-12 22:59:49 +0200

    Abstract Request Test initial

---
+++ 3.-Testing/3.1.-Shared/BlockBuster.Shared.Testing/AbstractRequestTest.cs
@@ -0,0 +1,39 @@
+using BlockBuster.Shared.Application.Bus.UseCase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using NUnit.Framework;
+
+namespace BlockBuster.Shared.Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetPageSizeAndNumberFromRequestUrlQuery()
+        {
+            var pageNumber = 3;
+            var pageSize = 25;
+            var number = "number";
+            var size = "size";
+
+            var query = new Mock<IQueryCollection>();
+            query.SetupGet(q => q["page[number]"]).Returns(new StringValues(pageNumber.ToString()));
+            query.SetupGet(q => q["page[size]"]).Returns(new StringValues(pageSize.ToString()));
+
+            var abstractRequest = new AbstractRequest(query.Object);
+            var pageProps = abstractRequest.Page();
+
+            Assert.AreEqual(pageProps.Keys.Count, 2);
+            Assert.IsTrue(pageProps.ContainsKey(number));
+            Assert.IsTrue(pageProps.ContainsKey(size));
+            Assert.AreEqual(pageProps[number], pageNumber);
+            Assert.AreEqual(pageProps[size], pageSize);
+            Assert.Pass();
+        }
+    }
+}
